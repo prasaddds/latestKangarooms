8d230a4364e65a5ff28f77387f2c67f5775bdecf
prasaddds <durgaprasad.78257@gmail.com>
2022-04-27 16:54:28 +0530

    origin

---
+++ src/Contacts.js
@@ -0,0 +1,53 @@
+const contacts = [
+    {
+      id:1,
+      name: "Double Bed Room",
+      pool: "No Swimming Pool",
+      wifi: "Yes",
+      theme: "Purple",
+      type: "Family",
+      available: "Available: 9",
+      booked: "Booked: 0",
+      imgURL:
+        "https://image.winudf.com/v2/image1/Y29tLndta2lkcy5saXZlY2FydG9vbl9zY3JlZW5fMV8xNTYwMDg1NzQxXzAxMw/screen-1.jpg?fakeurl=1&type=.jpg"
+    },
+    {
+      id:1,
+      name: "Double Bed Room",
+      pool: "No Swimming Pool",
+      wifi: "Yes",
+      theme: "Purple",
+      type: "Family",
+      available: "Available: 9",
+      booked: "Booked: 0",
+      imgURL:
+        "https://image.winudf.com/v2/image1/Y29tLndta2lkcy5saXZlY2FydG9vbl9zY3JlZW5fMV8xNTYwMDg1NzQxXzAxMw/screen-1.jpg?fakeurl=1&type=.jpg"
+    },
+    {
+      id:1,
+      name: "Double Bed Room",
+      pool: "No Swimming Pool",
+      wifi: "Yes",
+      theme: "Purple",
+      type: "Family",
+      available: "Available: 9",
+      booked: "Booked: 0",
+      imgURL:
+        "https://image.winudf.com/v2/image1/Y29tLndta2lkcy5saXZlY2FydG9vbl9zY3JlZW5fMV8xNTYwMDg1NzQxXzAxMw/screen-1.jpg?fakeurl=1&type=.jpg"
+    },
+    {
+      id:1,
+      name: "Double Bed Room",
+      pool: "No Swimming Pool",
+      wifi: "Yes",
+      theme: "Purple",
+      type: "Family",
+      available: "Available: 9",
+      booked: "Booked: 0",
+      imgURL:
+        "https://image.winudf.com/v2/image1/Y29tLndta2lkcy5saXZlY2FydG9vbl9zY3JlZW5fMV8xNTYwMDg1NzQxXzAxMw/screen-1.jpg?fakeurl=1&type=.jpg"
+    }
+  ];
+  
+  export default contacts;
+  
